8ff33934d8f2bb4d522dbd656eb3d2dca3c63314
Clairtonlm <clairtonlm@gmail.com>
2023-10-10 21:14:59 -0300

    Criando User

--- src/main/java/tech/clairtonlima/todoList/user/UserController.java
+++ src/main/java/tech/clairtonlima/todoList/user/UserController.java
@@ -1,16 +1,18 @@
 package tech.clairtonlima.todoList.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("meuTeste")
+@RequestMapping("users")
 public class UserController {
 
-     @GetMapping("/Teste")
-    public String mesagem(){
-        return "Testando pra ver se seu sei porraaaaa";
+     @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+       System.out.println(userModel.name);
     }
     
 }
---
+++ src/main/java/tech/clairtonlima/todoList/user/UserModel.java
@@ -0,0 +1,8 @@
+package tech.clairtonlima.todoList.user;
+
+public class UserModel {
+    
+    public String username;
+    public String name;
+    public String password;
+}
